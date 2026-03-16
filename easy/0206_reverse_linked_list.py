ed69bb514504003ddf45c1da257b6ea63a1e7821
gpna1229 <annaduen@gmail.com>
2026-03-17 01:19:01 +0800

    Update 0206_reverse_linked_list.py

--- easy/0206_reverse_linked_list.py
+++ easy/0206_reverse_linked_list.py
@@ -1,9 +1,10 @@
-# Recursion
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# Recursion
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
@@ -11,3 +12,15 @@ class Solution:
         else:
             self.reverseList(head.next)  
             return head      
+
+# Iteration
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr = head
+        new_head = None
+        while curr:
+            tmp = curr.next
+            curr.next = new_head
+            new_head = curr
+            curr = tmp 
+        return new_head
